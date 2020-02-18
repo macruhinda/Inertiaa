c7238695ab20e660894a35a0469d43a5cb95ecea
macruhinda <markruhinda47@gmail.com>
2020-02-18 18:00:52 +0300

    hello

---
+++ app/src/main/assets/addanumber.c
@@ -0,0 +1,16 @@
+//
+// Created by Agaba Mac Junior on 2020-02-18.
+//
+#include <stdio.h>
+
+int main(){
+int a,m,sum;
+
+printf("Enter a the Number");
+scanf("%d",&a);
+printf("Enter a the Number");
+scanf("%d",&m);
+sum=a+m;
+printf("Enter sum is %d", sum);
+return 0;
+}
--- app/src/main/java/com/example/inertia/ServiceMe.java
+++ app/src/main/java/com/example/inertia/ServiceMe.java
@@ -2,13 +2,25 @@ package com.example.inertia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ServiceMe extends AppCompatActivity {
-
+    MediaPlayer playMedia;
+Button player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_me);
+        player = findViewById(R.id.playbtn);
+        player.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playMedia= MediaPlayer.create(getApplicationContext(),R.raw.Inertiasong);
+                playMedia.start();
+            }
+        });
     }
 }
