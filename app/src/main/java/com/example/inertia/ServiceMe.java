4a4ccb80bc7b5fbdd3591f7fd345cee266cead5b
macruhinda <markruhinda47@gmail.com>
2020-02-19 09:56:55 +0300

    hello

--- app/src/main/java/com/example/inertia/ServiceMe.java
+++ app/src/main/java/com/example/inertia/ServiceMe.java
@@ -9,17 +9,27 @@ import android.widget.Button;
 
 public class ServiceMe extends AppCompatActivity {
     MediaPlayer playMedia;
-Button player;
+Button player,player1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_me);
         player = findViewById(R.id.playbtn);
+        player1 = findViewById(R.id.pausebtn);
+        player1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (playMedia != null && playMedia.isPlaying()) {
+                playMedia.stop();}
+
+            }
+        });
         player.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 playMedia= MediaPlayer.create(getApplicationContext(),R.raw.inertiasong);
                 playMedia.start();
+
             }
         });
     }
