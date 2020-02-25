b6c9e36673331c9f1efa577784774fc64ad4d31e
macruhinda <markruhinda47@gmail.com>
2020-02-25 19:15:39 +0300

    hello

--- app/src/main/java/com/example/inertia/MainActivity.java
+++ app/src/main/java/com/example/inertia/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.inertia;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,8 +21,6 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -37,11 +39,19 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater= getMenuInflater();
         inflater.inflate(R.menu.sohom, menu);
+
+        Button Lbutton=(Button)findViewById(R.id.toNamesList);
+        Lbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,TheList.class);
+                startActivity(intent);
+            }
+        });
         return true;
 
-
-
     }
+
     public  boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
             case R.id.Servicez:
