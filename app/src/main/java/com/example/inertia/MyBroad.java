fa56f7b30498452da9a3421089b979fd3573cc77
macruhinda <markruhinda47@gmail.com>
2020-12-12 22:37:15 +0300

    hello

--- app/src/main/java/com/example/inertia/MyBroad.java
+++ app/src/main/java/com/example/inertia/MyBroad.java
@@ -1,4 +1,24 @@
 package com.example.inertia;
 
-class MyBroad {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.view.Gravity;
+import android.widget.Toast;
+
+class MyBroad extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast toast= Toast.makeText(context,"Time Up",Toast.LENGTH_LONG);
+        //sets the position of the toast
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+        //calling the vibrator
+        Vibrator vibratoralarm;
+        vibratoralarm = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibratoralarm.vibrate(3000);
+
+
+    }
 }
